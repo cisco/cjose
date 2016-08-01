485e473e6057929bdaf494c757413b89b31f3a73
Hans Zandbelt <hzandbelt@pingidentity.com>
2016-08-01 19:16:33 +0200

    remove empty #if branch for HMAC_CTX_init call and negate expression

--- src/jws.c
+++ src/jws.c
@@ -324,10 +324,7 @@ static bool _cjose_jws_build_dig_hmac_sha(
         goto _cjose_jws_build_dig_hmac_sha_cleanup;
     }
 
-#if (CJOSE_OPENSSL_11X)
-    // do not remove this #if branch, but no further initialization of ctx is required here
-    // the HMAC_CTX_init call in the #else branch exists only in OpenSSL >= 1.1.0
-#else
+#if !(CJOSE_OPENSSL_11X)
     HMAC_CTX_init(ctx);
 #endif
 
